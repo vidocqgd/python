c4a0d7034d0e4d999f6f0d41e13d99de18cb8bfa
vidok <allu89@yandex.ru>
2023-03-10 16:35:36 +0300

    home1

---
+++ home1task2.py
@@ -0,0 +1,10 @@
+# Задача-2: Пользователь вводит время в секундах.
+# Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
+# Используйте форматирование строк.
+
+time_in_sec = int(input("Enter your local time in sec: "))
+hours = time_in_sec // 3600
+residue = time_in_sec % 3600
+minutes = residue // 60
+sec = residue % 60
+print(f"Now is {hours}:{minutes}:{sec} ")
