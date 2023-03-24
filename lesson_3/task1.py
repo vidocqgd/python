96cd250e4ee3be1152aba15bd5e93ac9961e9666
vidok <allu89@yandex.ru>
2023-03-24 15:50:19 +0300

    lesson5

--- lesson_3/task1.py
+++ lesson_3/task1.py
@@ -4,19 +4,19 @@
 
 seasons_list = ['winter', 'spring', 'summer', 'autumn']
 seasons_dict = {1 : 'winter', 2 : 'spring', 3 : 'summer', 4 : 'autumn'}
+
 month = int(input("print month number "))
 if month ==1 or month == 12 or month == 2:
-        print(seasons_dict.get(1))
-        print(seasons_list[0])
+    print(seasons_dict.get(1))
+    print(seasons_list[0])
 elif month == 3 or month == 4 or month ==5:
     print(seasons_dict.get(2))
     print(seasons_list[1])
 elif month == 6 or month == 7 or month == 8:
     print(seasons_dict.get(3))
     print(seasons_list[2])
-
 elif month == 9 or month == 10 or month == 11:
     print(seasons_dict.get(4))
     print(seasons_list[3])
 else:
-        print("There is no such month")
+    print("There is no such month")
